49517ff30404ee6f10444b52169c2257d0cffe94
nishina <n.toshi.1.014@gmail.com>
2017-01-07 19:32:31 +0900

    :sparkles: Complete triangle class

--- triangle.rb
+++ triangle.rb
@@ -1,20 +1,34 @@
-if ARGV.length != 3
-  puts '引数は3つにしてください。'
-  exit
+# triangle
+class Triangle
+  def initialize(sides)
+    @sides = sides.sort
+  end
+
+  def triangle_checker
+    if @sides[0] + @sides[1] <= @sides[2]
+      puts '三角形じゃないです＞＜'
+    elsif @sides.all? { |side| side == @sides[0] }
+      puts '正三角形ですね！'
+    elsif @sides[0] == @sides[1] || @sides[1] == @sides[2]
+      puts '二等辺三角形ですね！'
+    else
+      puts '不等辺三角形ですね！'
+    end
+  end
 end
 
-if ARGV.include?('0')
-  puts '0は入力しないでください。'
-  exit
+def main
+  if ARGV.length != 3
+    puts '引数は3つにしてください。'
+    exit
+  end
+  if ARGV.include?('0')
+    puts '0は入力しないでください。'
+    exit
+  end
+  input = ARGV.map(&:to_i)
+  triangle = Triangle.new(input)
+  triangle.triangle_checker
 end
 
-sides = ARGV.map(&:to_i).sort
-if sides[0] + sides[1] < sides[2]
-  puts '三角形じゃないです＞＜'
-elsif sides.all? { |side| side == sides[0] }
-  puts '二等辺三角形ですね！'
-elsif sides[0] == sides[1] && sides[1] == sides[2]
-  puts '正三角形ですね！'
-else
-  puts '不等辺三角形ですね！'
-end
+main
